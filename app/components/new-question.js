ccf23c1ca7e64c8557904c967bfb1f8d991d1895
Cameron Jensen <jensenseanc@gmail.com>
2017-04-28 10:30:23 -0700

    add new question template

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    },
+
+    saveQuestion() {
+      var params = {
+        question: this.get('question'),
+        author: this.get('author'),
+        notes: this.get('notes')
+      };
+      this.set('addNewQuestion', false);
+      this.sendAction('saveQuestion', params);
+    }
+  }
 });
